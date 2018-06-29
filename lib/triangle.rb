7f793a718e06c196f6037567bdaa826fb0daa758
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-06-29 20:27:00 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -11,6 +11,7 @@ def initialize(length_1,length_2,length_3)
 end
 
 def kind
+  validate_triangle
   if self.length_1 == self.length_2 && self.length_2 == self.length_3
     :equilateral
   elsif self.length_1 == self.length_2 || self.length_2 == self.length_3 || self.length_1 == self.length_3
@@ -21,6 +22,16 @@ def kind
 end
 
 def validate_triangle
+  triangle = [(length_1 + length_2 > length_3), (length_1 + length_3 > length_2), (length_2 + length_3 > length_1)]
+
+  if self.length_1 <= 0 || self.length_2 <= 0 || self.length_3 <= 0
+    
+  
+
+    
+      raise TriangleError
+
+    end
 end
 
 
