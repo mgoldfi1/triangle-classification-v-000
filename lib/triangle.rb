2016473d63aea1842626c8926057ec7786f51953
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-06-29 20:19:49 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -11,17 +11,7 @@ def initialize(length_1,length_2,length_3)
 end
 
 def kind
-  if self.length_1 <= 0 || self.length_2 <= 0 || self.length_3 <= 0
-    raise TriangleError
-  end
-  if (self.length_1 + self.length_2) < self.length_3 || (self.length_1 + self.length_3) < self.length_2 || (self.length_2 + self.length_3) < self.length_1
-
-    begin
-      raise TriangleError
-    rescue TriangleError => error
-      puts error.message
-    end
-  elsif self.length_1 == self.length_2 && self.length_2 == self.length_3
+  if self.length_1 == self.length_2 && self.length_2 == self.length_3
     :equilateral
   elsif self.length_1 == self.length_2 || self.length_2 == self.length_3 || self.length_1 == self.length_3
     :isosceles
