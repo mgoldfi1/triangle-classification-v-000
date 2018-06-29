91433f994c4ec5ee4df72ea1a39d93dfbdf78b4b
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-06-29 20:37:36 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -23,15 +23,8 @@ end
 
 def validate_triangle
   triangle = [(length_1 + length_2 > length_3), (length_1 + length_3 > length_2), (length_2 + length_3 > length_1)]
-
-  if self.length_1 <= 0 || self.length_2 <= 0 || self.length_3 <= 0
-
-
-
-
-      raise TriangleError
-
-    end
+  [length_1,length_2,length_3].each {|x| triangle << false if x <= 0}
+  raise TriangleError if triangle.include?(false)
 end
 
 
