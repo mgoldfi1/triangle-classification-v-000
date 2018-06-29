858b38bd5b17163a67cdb7e5320f9d8c14706bed
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-06-29 16:47:18 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,15 @@
 class Triangle
   # write code here
+  attr_accessor :length_1, :length_2, :length_3
+
+
+def initialize(length_1,length_2,length_3)
+  @length_1 = length_1
+  @length_2 = length_2
+  @length_3 = length_3
+end
+
+
+
+
 end
